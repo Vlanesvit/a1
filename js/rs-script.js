c362c2921c369d04a294701de7e922498795246e
Vlad Nesterov <53149984+Vlanesvit@users.noreply.github.com>
2024-09-06 17:47:39 +0800

    vk video

--- js/rs-script.js
+++ js/rs-script.js
@@ -225,6 +225,293 @@ if (document.querySelector('[data-spollers]')) {
 	spollers()
 }
 
+(() => {
+	"use strict";
+	const modules = {};
+	class Popup {
+		constructor(options) {
+			let config = {
+				logging: true,
+				init: true,
+				// Для кнопок 
+				attributeOpenButton: 'data-popup', // Атрибут для кнопки, которая вызывает попап
+				attributeCloseButton: 'data-close', // Атрибут для кнопки, которая закрывает попап
+				// Для сторонних объектов
+				fixElementSelector: '[data-lp]', // Атрибут для элементов с левым паддингом (которые fixed)
+				// Для объекта попапа
+				videoAttribute: 'data-popup-video', // Универсальный атрибут для кода видео
+				videoPlaceAttribute: 'data-popup-video-place', // Атрибут для вставки iframe видео
+				setAutoplayVideo: true,
+				// Изменение классов
+				classes: {
+					popup: 'popup',
+					popupContent: 'popup__content',
+					popupActive: 'popup_show', // Добавляется для попапа, когда он открывается
+					bodyActive: 'popup-show', // Добавляется для боди, когда попап открыт
+				},
+				focusCatch: false, // Фокус внутри попапа зациклен
+				closeEsc: true, // Закрытие по ESC
+				bodyLock: true, // Блокировка скролла
+				hashSettings: {
+					location: true, // Хэш в адресной строке
+					goHash: true, // Переход по наличию в адресной строке
+				},
+				on: { // События
+					beforeOpen: function () { },
+					afterOpen: function () { },
+					beforeClose: function () { },
+					afterClose: function () { },
+				},
+			}
+			this.videoCode;
+			this.isOpen = false;
+			this.targetOpen = { selector: false, element: false };
+			this.previousOpen = { selector: false, element: false };
+			this.lastClosed = { selector: false, element: false };
+			this._dataValue = false;
+			this.hash = false;
+			this._reopen = false;
+			this._selectorOpen = false;
+			this.lastFocusEl = false;
+			this._focusEl = [
+				'a[href]', 'input:not([disabled]):not([type="hidden"]):not([aria-hidden])',
+				'button:not([disabled]):not([aria-hidden])', 'select:not([disabled]):not([aria-hidden])',
+				'textarea:not([disabled]):not([aria-hidden])', 'area[href]', 'iframe',
+				'object', 'embed', '[contenteditable]', '[tabindex]:not([tabindex^="-"])'
+			];
+			this.options = {
+				...config,
+				...options,
+				classes: { ...config.classes, ...options?.classes },
+				hashSettings: { ...config.hashSettings, ...options?.hashSettings },
+				on: { ...config.on, ...options?.on }
+			};
+			this.bodyLock = false;
+			this.options.init ? this.initPopups() : null;
+		}
+
+		initPopups() {
+			this.eventsPopup();
+		}
+
+		eventsPopup() {
+			document.addEventListener("click", function (e) {
+				const buttonOpen = e.target.closest(`[${this.options.attributeOpenButton}]`);
+				if (buttonOpen) {
+					e.preventDefault();
+					this._dataValue = buttonOpen.getAttribute(this.options.attributeOpenButton) || 'error';
+					this.videoCode = buttonOpen.getAttribute(this.options.videoAttribute) || null;
+					if (this._dataValue !== 'error') {
+						if (!this.isOpen) this.lastFocusEl = buttonOpen;
+						this.targetOpen.selector = `${this._dataValue}`;
+						this._selectorOpen = true;
+						this.open();
+					}
+					return;
+				}
+
+				const buttonClose = e.target.closest(`[${this.options.attributeCloseButton}]`);
+				if (buttonClose || (!e.target.closest(`.${this.options.classes.popupContent}`) && this.isOpen)) {
+					e.preventDefault();
+					this.close();
+					return;
+				}
+			}.bind(this));
+
+			document.addEventListener("keydown", function (e) {
+				if (this.options.closeEsc && e.code === 'Escape' && this.isOpen) {
+					e.preventDefault();
+					this.close();
+				}
+				if (this.options.focusCatch && e.code === 'Tab' && this.isOpen) {
+					this._focusCatch(e);
+				}
+			}.bind(this));
+
+			if (this.options.hashSettings.goHash) {
+				window.addEventListener('hashchange', function () {
+					if (window.location.hash) {
+						this._openToHash();
+					} else {
+						this.close(this.targetOpen.selector);
+					}
+				}.bind(this));
+
+				window.addEventListener('load', function () {
+					if (window.location.hash) {
+						this._openToHash();
+					}
+				}.bind(this));
+			}
+		}
+
+		open(selectorValue) {
+			if (bodyLockStatus) {
+				this.bodyLock = document.documentElement.classList.contains('lock') && !this.isOpen;
+
+				if (selectorValue && typeof selectorValue === "string" && selectorValue.trim() !== "") {
+					this.targetOpen.selector = selectorValue;
+					this._selectorOpen = true;
+				}
+				if (this.isOpen) {
+					this._reopen = true;
+					this.close();
+				}
+				if (!this._selectorOpen) this.targetOpen.selector = this.lastClosed.selector;
+				if (!this._reopen) this.previousActiveElement = document.activeElement;
+
+				this.targetOpen.element = document.querySelector(this.targetOpen.selector);
+
+				if (this.targetOpen.element) {
+					if (this.videoCode) {
+						const videoSrc = this._getVideoSrc(this.videoCode);
+						this._insertVideo(videoSrc);
+					}
+
+					if (this.options.hashSettings.location) {
+						this._getHash();
+						this._setHash();
+					}
+
+					this.options.on.beforeOpen(this);
+					document.dispatchEvent(new CustomEvent("beforePopupOpen", { detail: { popup: this } }));
+
+					this.targetOpen.element.classList.add(this.options.classes.popupActive);
+					document.documentElement.classList.add(this.options.classes.bodyActive);
+
+					!this._reopen ? (!this.bodyLock ? bodyLock() : null) : this._reopen = false;
+
+					this.targetOpen.element.setAttribute('aria-hidden', 'false');
+
+					this.previousOpen.selector = this.targetOpen.selector;
+					this.previousOpen.element = this.targetOpen.element;
+
+					this._selectorOpen = false;
+					this.isOpen = true;
+
+					setTimeout(() => { this._focusTrap(); }, 50);
+
+					this.options.on.afterOpen(this);
+					document.dispatchEvent(new CustomEvent("afterPopupOpen", { detail: { popup: this } }));
+				}
+			}
+		}
+
+		close(selectorValue) {
+			if (selectorValue && typeof selectorValue === "string" && selectorValue.trim() !== "") {
+				this.previousOpen.selector = selectorValue;
+			}
+			if (!this.isOpen || !bodyLockStatus) {
+				return;
+			}
+
+			this.options.on.beforeClose(this);
+			document.dispatchEvent(new CustomEvent("beforePopupClose", { detail: { popup: this } }));
+
+			if (this.videoCode) {
+				const videoContainer = this.targetOpen.element.querySelector(`[${this.options.videoPlaceAttribute}]`);
+				if (videoContainer) videoContainer.innerHTML = '';
+			}
+
+			this.previousOpen.element.classList.remove(this.options.classes.popupActive);
+			this.previousOpen.element.setAttribute('aria-hidden', 'true');
+
+			if (!this._reopen) {
+				document.documentElement.classList.remove(this.options.classes.bodyActive);
+				!this.bodyLock ? bodyUnlock() : null;
+				this.isOpen = false;
+			}
+
+			this._removeHash();
+
+			if (this._selectorOpen) {
+				this.lastClosed.selector = this.previousOpen.selector;
+				this.lastClosed.element = this.previousOpen.element;
+			}
+
+			this.options.on.afterClose(this);
+			document.dispatchEvent(new CustomEvent("afterPopupClose", { detail: { popup: this } }));
+
+			setTimeout(() => { this._focusTrap(); }, 50);
+		}
+
+		_getHash() {
+			if (this.options.hashSettings.location) {
+				this.hash = this.targetOpen.selector.includes('#') ?
+					this.targetOpen.selector : this.targetOpen.selector.replace('.', '#');
+			}
+		}
+
+		_openToHash() {
+			let classInHash = document.querySelector(`.${window.location.hash.replace('#', '')}`) ? `.${window.location.hash.replace('#', '')}` :
+				document.querySelector(`${window.location.hash}`) ? `${window.location.hash}` :
+					null;
+
+			const buttons = document.querySelector(`[${this.options.attributeOpenButton}="${classInHash}"]`) || document.querySelector(`[${this.options.attributeOpenButton}="${classInHash.replace('.', "#")}"]`);
+			if (buttons && classInHash) this.open(classInHash);
+		}
+
+		_setHash() {
+			history.pushState('', '', this.hash);
+		}
+
+		_removeHash() {
+			history.pushState('', '', window.location.href.split('#')[0]);
+		}
+
+		_focusCatch(e) {
+			const focusable = this.targetOpen.element.querySelectorAll(this._focusEl);
+			const focusArray = Array.prototype.slice.call(focusable);
+			const focusedIndex = focusArray.indexOf(document.activeElement);
+			if (e.shiftKey && focusedIndex === 0) {
+				focusArray[focusArray.length - 1].focus();
+				e.preventDefault();
+			}
+			if (!e.shiftKey && focusedIndex === focusArray.length - 1) {
+				focusArray[0].focus();
+				e.preventDefault();
+			}
+		}
+
+		_focusTrap() {
+			const focusable = this.previousOpen.element.querySelectorAll(this._focusEl);
+			if (!this.isOpen) this.lastFocusEl ? this.lastFocusEl.focus() : null;
+			else focusable[0].focus();
+		}
+
+		_insertVideo(videoSrc) {
+			const videoContainer = this.targetOpen.element.querySelector(`[${this.options.videoPlaceAttribute}]`);
+			if (videoContainer) {
+				videoContainer.innerHTML = `<iframe src="${videoSrc}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>`;
+			}
+		}
+
+		_getVideoSrc(videoCode) {
+			if (videoCode.includes("youtube.com") || videoCode.includes("youtu.be")) {
+				return `https://www.youtube.com/embed/${this._extractYouTubeCode(videoCode)}?autoplay=1`;
+			} else if (videoCode.includes("vk.com")) {
+				return `https://vk.com/video_ext.php?${this._extractVKCode(videoCode)}&autoplay=1`;
+			}
+			return videoCode;
+		}
+
+		_extractYouTubeCode(url) {
+			const regExp = /(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
+			const match = url.match(regExp);
+			return (match && match[1]) ? match[1] : null;
+		}
+
+		_extractVKCode(url) {
+			const regExp = /video_ext.php\?([^"]+)/;
+			const match = url.match(regExp);
+			return (match && match[1]) ? match[1] : null;
+		}
+	}
+
+	modules.popup = new Popup({});
+})();
+
+
 /* ====================================
 Рейтинг
 ==================================== */
